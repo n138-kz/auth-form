54d47e8d48b5df3191dc6dc8673451de4fbe3ca5
Yuu Komiya <8064928+n138-kz@users.noreply.github.com>
2026-08-07 15:44:17 +0900

    Update login.php

--- docs/login.php
+++ docs/login.php
@@ -4,3 +4,7 @@ session_start([
     'cookie_lifetime' => 86400,
     'use_strict_mode' => true,
 ]);
+
+header('Content-Type: application/json; charset=UTF-8');
+$input = json_decode(file_get_contents('php://input'), true);
+json_encode($input, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT);
