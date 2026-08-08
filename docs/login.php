106acf428c3ae0955fcc25862dc8d6ace0f2cbc9
Yuu Komiya <8064928+n138-kz@users.noreply.github.com>
2026-08-08 09:35:53 +0900

    Update login.php

--- docs/login.php
+++ docs/login.php
@@ -38,7 +38,7 @@ $input = json_decode(file_get_contents('php://input'), true);
     ]);
     curl_setopt($curl_req, CURLOPT_RETURNTRANSFER, true);
     $curl_result = json_decode(curl_exec($curl_req), true);
-    echo json_encode($curl_result, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT);
+    echo json_encode($curl_result, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT).PHP_EOL;
 
     {
         /* *https://zenn.dev/niisan/articles/cb3cedeeaf3ed7* */
@@ -65,7 +65,7 @@ $input = json_decode(file_get_contents('php://input'), true);
         ]);
         curl_setopt($curl_req, CURLOPT_RETURNTRANSFER, true);
         $curl_result = json_decode(curl_exec($curl_req), true);
-        echo json_encode($curl_result, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT);
+        echo json_encode($curl_result, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT).PHP_EOL;
     }
 
 }
