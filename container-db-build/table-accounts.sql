bddc2ffcbbc8876aee40c103d43f20d461873f34
Yuu Komiya <8064928+n138-kz@users.noreply.github.com>
2026-08-08 19:07:18 +0900

    Update table-accounts.sql

--- container-db-build/table-accounts.sql
+++ container-db-build/table-accounts.sql
@@ -80,7 +80,9 @@ CREATE VIEW accounts_view_unsafe AS
     FROM
     accounts a
 LEFT JOIN 
-    accounts_otp o ON a.id = o.account_id;
+    accounts_otp o ON a.id = o.account_id
+LEFT JOIN
+    accounts_attr attr ON a.id = attr.account_id;
 CREATE VIEW accounts_view AS
     SELECT
         a.id,
@@ -96,5 +98,7 @@ CREATE VIEW accounts_view AS
     FROM
     accounts a
 LEFT JOIN 
-    accounts_otp o ON a.id = o.account_id;
+    accounts_otp o ON a.id = o.account_id
+LEFT JOIN
+    accounts_attr attr ON a.id = attr.account_id;
 INSERT INTO accounts(userid, password_hash) VALUES ('admin@localhost', 'U2FsdGVkX188L2MufK+yMWuLKmSAtuWtnP+Q4MGyusU='); -- admin@localhost / password
