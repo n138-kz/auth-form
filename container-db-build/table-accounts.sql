25fe207666c53d89906654eceb9c4a4a5d35b948
Yuu Komiya <8064928+n138-kz@users.noreply.github.com>
2026-08-08 17:10:23 +0900

    Update table-accounts.sql

--- container-db-build/table-accounts.sql
+++ container-db-build/table-accounts.sql
@@ -40,4 +40,4 @@ CREATE VIEW accounts_view AS
     accounts a
 LEFT JOIN 
     accounts_otp o ON a.id = o.account_id;
-INSERT INTO accounts(userid, password_hash) VALUES ('admin@localhost', 'U2FsdGVkX188L2MufK+yMWuLKmSAtuWtnP+Q4MGyusU=');
+INSERT INTO accounts(userid, password_hash) VALUES ('admin@localhost', 'U2FsdGVkX188L2MufK+yMWuLKmSAtuWtnP+Q4MGyusU='); -- admin@localhost / password
