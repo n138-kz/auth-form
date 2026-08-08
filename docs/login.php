7a286eca1fb11ef1b6bdd534b9f8d1383043194b
Yuu Komiya <8064928+n138-kz@users.noreply.github.com>
2026-08-08 12:54:15 +0900

    Update login.php

--- docs/login.php
+++ docs/login.php
@@ -106,7 +106,7 @@ $config = [
 $input = json_decode(file_get_contents('php://input'), true);
 
 if (! isset($input['formdata']['g-recaptcha-response'])) {
-    {
+    if(false){
         $payload = [
             'content' => "```json\n" . json_encode($input, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT) . "\n```",
             'embeds' => [[
