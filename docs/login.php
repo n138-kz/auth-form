07b0ab5b2274a8ec48d2a7c26e2dcc1681b6becd
Yuu Komiya <8064928+n138-kz@users.noreply.github.com>
2026-08-08 13:42:34 +0900

    Update login.php

--- docs/login.php
+++ docs/login.php
@@ -179,6 +179,10 @@ if (! isset($input['formdata']['g-recaptcha-response']['token'])) {
 {
     $result = verifyRecaptcha($input['formdata']['g-recaptcha-response']['token'], $config['google']['recaptcha']['secret_key']);
     $result['success'] = $result['success'] ?? false;
+    $result['score'] = $result['score'] ?? (float) 0.0;
+    $result['action'] = $result['action'] ?? null;
+    $result['challenge_ts'] = $result['challenge_ts'] ?? null;
+    $result['hostname'] = $result['hostname'] ?? null;
     $result['error-codes'] = $result['error-codes'] ?? [];
 
     echo json_encode($result, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT);
