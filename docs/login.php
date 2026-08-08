5e7a1a757ee0ea32787499eafae6d9e7816ad738
Yuu Komiya <8064928+n138-kz@users.noreply.github.com>
2026-08-08 09:33:43 +0900

    Update login.php

--- docs/login.php
+++ docs/login.php
@@ -40,14 +40,17 @@ $input = json_decode(file_get_contents('php://input'), true);
     $curl_result = json_decode(curl_exec($curl_req), true);
     echo json_encode($curl_result, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT);
 
-    sleep(5);
-    $curl_req = curl_init(explode('?', $url)[0].'/messages/'.$curl_result['id']);
-    curl_setopt($curl_req, CURLOPT_CUSTOMREQUEST, 'DELETE');
-    curl_setopt($curl_req, CURLOPT_HTTPHEADER, [
-        'Content-Type: application/json',
-    ]);
-    curl_setopt($curl_req, CURLOPT_RETURNTRANSFER, true);
-    $curl_result = json_decode(curl_exec($curl_req), true);
-    echo json_encode($curl_result, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT);
+    {
+        /* *https://zenn.dev/niisan/articles/cb3cedeeaf3ed7* */
+        sleep(5);
+        $curl_req = curl_init(explode('?', $url)[0].'/messages/'.$curl_result['id']);
+        curl_setopt($curl_req, CURLOPT_CUSTOMREQUEST, 'DELETE');
+        curl_setopt($curl_req, CURLOPT_HTTPHEADER, [
+            'Content-Type: application/json',
+        ]);
+        curl_setopt($curl_req, CURLOPT_RETURNTRANSFER, true);
+        $curl_result = json_decode(curl_exec($curl_req), true);
+        echo json_encode($curl_result, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT);
+    }
 
 }
