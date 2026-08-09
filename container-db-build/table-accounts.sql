799d3cfc7f6864e468e6da8ecc0644f931a1095a
Yuu Komiya <8064928+n138-kz@users.noreply.github.com>
2026-08-09 14:26:30 +0900

    Update table-accounts.sql

--- container-db-build/table-accounts.sql
+++ container-db-build/table-accounts.sql
@@ -98,9 +98,7 @@ CREATE OR REPLACE VIEW accounts_view AS
     FROM
         accounts a
     LEFT JOIN 
-        accounts_otp o
-        ON
-        a.id = o.account_id
+        accounts_otp o ON a.id = o.account_id
     LEFT JOIN
         accounts_attr attr ON a.id = attr.account_id;
 CREATE OR REPLACE VIEW accounts_view_candidate AS
