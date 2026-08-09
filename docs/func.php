3290a98a0fd6ade3f6377caf73d24729432eba83
Yuu Komiya <8064928+n138-kz@users.noreply.github.com>
2026-08-09 18:50:50 +0900

    Update func.php

--- docs/func.php
+++ docs/func.php
@@ -49,7 +49,9 @@ function sendDiscordWebhook(string $webhookUrl = '', array $payload = ['content'
                 ]);
                 $stm = $pdo -> prepare('INSERT INTO webhook_discord(m_response) VALUES (:response)');
                 $stm -> execute([':response' => json_encode($curl_result)]);
-            } catch (\Exception $e) {}
+            } catch (\Exception $e) {
+                error_log($e->getMessage());
+            }
         } else {
             error_log('Database parametors has empty.');
         }
