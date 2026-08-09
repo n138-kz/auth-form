40254edf5992ba9c72daa76894adc8b17bca1f0b
Yuu Komiya <8064928+n138-kz@users.noreply.github.com>
2026-08-09 18:59:16 +0900

    Update signup.php

--- docs/signup.php
+++ docs/signup.php
@@ -338,12 +338,12 @@ if(false) {
                                 'Mail message: ' . ($mail['description'] ?? null) . "\n" .
                                 'Mail sendto: ' . ($mail['sendto'] ?? null),
                             'fields' => [
-                                [ 'name' => 'Timestamp', value: '<t:' . time() . ':F> <t:' . time() . ':R>' ],
-                                [ 'name' => 'IPアドレス', value: ($_SERVER['REMOTE_ADDR'] ?? null) ],
-                                [ 'name' => 'User-Agent', value: ($_SERVER['HTTP_USER_AGENT'] ?? null) ],
-                                [ 'name' => 'Referer', value: ($_SERVER['HTTP_REFERER'] ?? null) ],
-                                [ 'name' => 'Mail Sendto', value: ($mail['sendto'] ?? null) ],
-                                [ 'name' => 'Mail Message', value: ($mail['description'] ?? null) ],
+                                [ 'name' => 'Timestamp', 'value' => '<t:' . time() . ':F> <t:' . time() . ':R>' ],
+                                [ 'name' => 'IPアドレス', 'value' => ($_SERVER['REMOTE_ADDR'] ?? null) ],
+                                [ 'name' => 'User-Agent', 'value' => ($_SERVER['HTTP_USER_AGENT'] ?? null) ],
+                                [ 'name' => 'Referer', 'value' => ($_SERVER['HTTP_REFERER'] ?? null) ],
+                                [ 'name' => 'Mail Sendto', 'value' => ($mail['sendto'] ?? null) ],
+                                [ 'name' => 'Mail Message', 'value' => ($mail['description'] ?? null) ],
                             ],
                             'color' => $mail['code']===1 ? hexdec('FF0000') : hexdec('006400'),
                             'timestamp' => date('c'),
