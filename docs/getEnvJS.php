2b47ff763bf0b9c6271609bc032138a12d1a3299
Yuu Komiya <8064928+n138-kz@users.noreply.github.com>
2026-08-09 16:56:43 +0900

    config.discord.authn_bot

--- docs/getEnvJS.php
+++ docs/getEnvJS.php
@@ -19,8 +19,10 @@ echo json_encode([
     ],
     'discord' => [
         'webhook_url' => getenv('DISCORD_WEBHOOK_URL') ?? null,
-        'client_id' => getenv('DISCORD_AUTHN_BOT_CLIENT_ID') ?? null,
-        'redirect_uri' => getenv('DISCORD_AUTHN_BOT_REDIRECT_URI') ?? null,
-        'scope' => getenv('DISCORD_AUTHN_BOT_SCOPE') ?? null,
+        'authn_bot' => [
+            'client_id' => getenv('DISCORD_AUTHN_BOT_CLIENT_ID') ?? null,
+            'redirect_uri' => getenv('DISCORD_AUTHN_BOT_REDIRECT_URI') ?? null,
+            'scope' => getenv('DISCORD_AUTHN_BOT_SCOPE') ?? null,
+        ],
     ],
 ]);
