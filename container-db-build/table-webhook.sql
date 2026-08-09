9f7e539715379057f4f619df3021eb7362528234
Yuu Komiya <8064928+n138-kz@users.noreply.github.com>
2026-08-09 18:36:10 +0900

    Create table-webhook.sql

---
+++ container-db-build/table-webhook.sql
@@ -0,0 +1,16 @@
+DROP TABLE IF EXISTS webhook_discord;
+CREATE TABLE IF NOT EXISTS webhook_discord (
+    id SERIAL PRIMARY KEY
+    ,created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+    ,updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+    ,m_id                   VARCHAR(255)
+    ,m_channel_id           VARCHAR(255)
+    ,m_author_id            VARCHAR(255)
+    ,m_author_username      VARCHAR(255)
+    ,m_author_bot           BOOLEAN DEFAULT FALSE
+    ,m_author_global_name   VARCHAR(255)
+    ,m_pinned               BOOLEAN DEFAULT FALSE
+    ,m_mention_everyone     BOOLEAN DEFAULT FALSE
+    ,m_webhook_id           VARCHAR(255)
+    ,m_response             TEXT
+);
