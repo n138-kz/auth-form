2711028ca64ddd8683e39611f462655e422929bf
Yuu Komiya <8064928+n138-kz@users.noreply.github.com>
2026-08-09 18:40:17 +0900

    Update func.php

--- docs/func.php
+++ docs/func.php
@@ -43,6 +43,7 @@ function sendDiscordWebhook(string $webhookUrl = '', array $payload = ['content'
             &&
             isset($config['user']) && isset($config['pass'])
         ) {
+            $dsn = "pgsql:host={$config['host']};port={$config['port']};dbname={$config['db']}";
         }
     }
 
