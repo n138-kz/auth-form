368061599e2fdbdda062645dfdbe68fbecc32d50
Yuu Komiya <8064928+n138-kz@users.noreply.github.com>
2026-08-09 17:52:39 +0900

    Update func.php

--- docs/func.php
+++ docs/func.php
@@ -1,6 +1,10 @@
 <?php
 ini_set('display_errors', 0);
 function sendDiscordWebhook(string $webhookUrl, array $payload) {
+    if (empty($payload)) {
+        return;
+    }
+
     $url = $webhookUrl . '?wait=true';
     $payload = json_encode($payload, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT);
 
