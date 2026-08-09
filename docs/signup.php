236355aae6b85f3a601ce6d8bdc011e5d3bef5a9
Yuu Komiya <8064928+n138-kz@users.noreply.github.com>
2026-08-09 10:17:37 +0900

    Update signup.php

--- docs/signup.php
+++ docs/signup.php
@@ -259,7 +259,7 @@ if(false) {
                 ];
                 $mailbody['subject'] = 'ようこそ: アカウント本登録のお願い' . date('c');
                 $mailbody['content'] = "こんにちは！n138.jpです。\nこのメールはアカウント仮登録が完了したことをお知らせするメールです。\n\nまだ本登録は完了していません。\n\n6時間以内に本登録が完了しない場合、仮登録は削除されます。\n" . date('c') . "";
-                $mailbody['ishtml'] = $mailbody['ishtml'] ? $mailbody['ishtml'] : false;
+                $mailbody['ishtml'] = false;
 
                 $mail = sendMail($mailattr, $mailbody);
                 {
